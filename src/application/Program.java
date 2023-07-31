cd5b72979010390a9cd3827fb33f079c0a2fb784
Michel Klein <michel.kps@outlook.com>
2023-07-31 11:58:19 -0300

    feat: findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,13 +6,18 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        List<Seller> sellerList = sellerDao.findByDepartment(2);
+        for (Seller sellerItem : sellerList) {
+            System.out.println(sellerItem);
+        }
     }
 }
